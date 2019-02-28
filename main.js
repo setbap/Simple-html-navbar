33bdf47b8bc13ca3194d5b817e4d55d1293ea5bb
Sina <ebr.sina@outlook.com>
2019-02-28 23:24:09 +0330

    created :)

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".nav-toggle").addEventListener("click", (a) => {
+	document.querySelector(".nav").classList.toggle("deactive");
+	console.log(document.querySelector(".nav").classList);
+});
